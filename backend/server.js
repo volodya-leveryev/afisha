5309b7f2acfeffb2f2a030a4f085701effea08a7
Vladimir Leveryev <volodya.leveryev@gmail.com>
2022-06-18 00:24:16 +0900

    Connect to Db

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 var express = require("express");
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cors = require("cors");
 var router = express.Router();
 var session = require('express-session');
-// var passport = require("passport");
-// var passportLocalMongoose = require("passport-local-mongoose");
+var passport = require("passport");
+var passportLocalMongoose = require("passport-local-mongoose");
 
 var app = express();
 
@@ -24,87 +24,87 @@ app.use(session({
     saveUninitialized: false
 }));
   
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 // //Connecting to MongoDB using mongoose
-// mongoose.createConnection('mongodb://localhost:27017/afisha').asPromise();
+mongoose.createConnection('mongodb://localhost:27017/afisha').asPromise();
 
-// //Defining Schemas
-// var userSchema = new mongoose.Schema({
-//     fullName: String,
-//     username: String,
-//     contactNumber: String,
-//     password: String
-// });
-// var bookingSchema = new mongoose.Schema({
-//     booking: String
-// });
-
-// //Using passport-local-mongoose plugin on userSchema
-// userSchema.plugin(passportLocalMongoose);
-
-// //Defining Models
-// var User = new mongoose.model('User', userSchema);
-// var Booking = new mongoose.model('Booking', bookingSchema);
-
-// passport.use(User.createStrategy());
-
-// passport.serializeUser(function(user, done) {
-//   done(null, user.id);
-// });
-
-// passport.deserializeUser(function(id, done) {
-//   User.findById(id, function(err, user) {
-//     done(err, user);
-//   });
-// });
-
-// //All our requests go in here
-router.get('/signedin', function(req, res){
-//     res.json(req.isAuthenticated());
-    res.json({message:"hello"})
+//Defining Schemas
+var userSchema = new mongoose.Schema({
+    fullName: String,
+    username: String,
+    contactNumber: String,
+    password: String
+});
+var bookingSchema = new mongoose.Schema({
+    booking: String
 });
 
-// router.post('/signin', function(req, res){
-//     const user = new User({
-//         username: req.body.username,
-//         password: req.body.password
-//     });
+//Using passport-local-mongoose plugin on userSchema
+userSchema.plugin(passportLocalMongoose);
 
-//     req.login(user, function(err){
-//         if (err) {
-//             console.log(err);
-//         }
-//         else {
-//             passport.authenticate("local")(req, res, function(){
-//                 res.send(user);
-//             });
-//         }
-//     });
-// });
+//Defining Models
+var User = new mongoose.model('User', userSchema);
+var Booking = new mongoose.model('Booking', bookingSchema);
 
-// router.post('/register', function(req, res){
-//     User.register({fullName: req.body.fullName, 
-//         username: req.body.username,
-//         contactNumber: req.body.contactNumber}, req.body.password, function(err, user){
-//             passport.authenticate("local")(req, res, function(){
-//                 res.json(user);
-//         });
-//     });
-// });
+passport.use(User.createStrategy());
 
-// router.post('/pay', function(req, res){
-//     var newBooking = Booking({booking: req.body.bookingDetails}).save(function(err, data){
-//         if(err){
-//             console.log(err);
-//         }
-//         else{
-//             res.json(data);
-//         }
-//     });
-// });
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+//All our requests go in here
+router.get('/signedin', function(req, res){
+    res.json(req.isAuthenticated());
+    // res.json({message:"hello"})
+});
+
+router.post('/signin', function(req, res){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err){
+        if (err) {
+            console.log(err);
+        }
+        else {
+            passport.authenticate("local")(req, res, function(){
+                res.send(user);
+            });
+        }
+    });
+});
+
+router.post('/register', function(req, res){
+    User.register({fullName: req.body.fullName, 
+        username: req.body.username,
+        contactNumber: req.body.contactNumber}, req.body.password, function(err, user){
+            passport.authenticate("local")(req, res, function(){
+                res.json(user);
+        });
+    });
+});
+
+router.post('/pay', function(req, res){
+    var newBooking = Booking({booking: req.body.bookingDetails}).save(function(err, data){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.json(data);
+        }
+    });
+});
 
 // router.post('/seats', function(req, res){
  
@@ -131,10 +131,10 @@ router.get('/signedin', function(req, res){
 //     });*/
 // });
 
-// router.get('/logout', function(req, res){
-//     req.logout();
-//     res.json(req.isAuthenticated());
-// });
+router.get('/logout', function(req, res){
+    req.logout();
+    res.json(req.isAuthenticated());
+});
 
 app.use('/api', router);
 
